825ff209bfd80c4fe4e8e0f3521ba52fcb4a54a1
BigWales98 <gameliker68@gmail.com>
2024-09-26 17:33:07 +0900

    글자폰트수정

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,14 @@
-import React from 'react'
+import React from 'react';
 
-export default function Home() 
-{
-  return <div><strong>Welcome Home</strong></div>
+export default function Home() {
+  return (
+    <div style={{
+      display: 'flex', 
+      justifyContent: 'center', 
+      alignItems: 'center', 
+      height: '100vh'
+    }}>
+      <strong style={{ fontSize: '2rem' }}>Welcome Home</strong>
+    </div>
+  );
 }
